67db49488e69b0978be61ff1dd49ec34b1e9372b
Ilya Khrebtov <zkhrebtovilya@gmail.com>
2022-06-12 23:51:08 +0600

    Добавлена поддержка CORS

--- pages/api/Register.js
+++ pages/api/Register.js
@@ -1,3 +1,4 @@
+import Cors from 'cors';
 import { NextApiRequest, NextApiResponse } from 'next';
 import nextSession from "next-session";
 export const getSession = nextSession();
@@ -5,7 +6,25 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const md5 = require('md5');
 
+const cors = Cors({
+    methods: ['GET', 'HEAD', 'POST'],
+});
+
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result) => {
+            if (result instanceof Error) {
+                return reject(result);
+            }
+
+            return resolve(result);
+        });
+    });
+}
+
 export default async function Register(req, res) {
+    await runMiddleware(req, res, cors);
+
     if (req.method === 'POST') {
         if (!req.body['login'] || !req.body['password']) {
             res.status(403).send('Not found login or password');
